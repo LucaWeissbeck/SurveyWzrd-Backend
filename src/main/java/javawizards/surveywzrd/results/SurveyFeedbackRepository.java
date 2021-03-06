11c40af0cdd9d9697aabbec2fdb680dc2d77c3ef
Kathrin <elja2043@gmail.com>
2021-03-06 12:07:34 +0100

    Survey-Feedback Class (+DB) + -Controller + -Repository; Table Columns tbd!

---
+++ src/main/java/javawizards/surveywzrd/results/SurveyFeedbackRepository.java
@@ -0,0 +1,10 @@
+package javawizards.surveywzrd.results;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SurveyFeedbackRepository extends CrudRepository<SurveyFeedback, Long> {
+    SurveyFeedback findById(long id);
+    List<SurveyFeedback> findBySurvey_Id(long id);
+}
