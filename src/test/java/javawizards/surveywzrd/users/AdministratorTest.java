6b7caa88f5678c740d2e155df4f737976c0f8cc9
Mauritz Merk <wi19181@lehre.dhbw-stuttgart.de>
2021-03-17 11:59:47 +0100

    Commit full test for Administrator class.

--- src/test/java/javawizards/surveywzrd/users/AdministratorTest.java
+++ src/test/java/javawizards/surveywzrd/users/AdministratorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdministratorTest {
@@ -19,42 +21,142 @@ class AdministratorTest {
     }
 
     @Test
-    void getId() {
+    void getId() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Administrator pojo = new Administrator();
+        final Field field = pojo.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(pojo, 123l);
+
+        //when
+        final Long result = pojo.getId();
+
+        //then
+        assertEquals(123l, result, "magic_values");
     }
 
     @Test
-    void setId() {
+    void setId() throws NoSuchFieldException, IllegalAccessException {
+        final Administrator pojo = new Administrator();
+
+        //when
+        pojo.setId(123l);
+
+        //then
+        final Field field = pojo.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        assertEquals(123l, field.get(pojo), "foo");
     }
 
     @Test
-    void getFirstName() {
+    void getFirstName() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Administrator pojo = new Administrator();
+        final Field field = pojo.getClass().getDeclaredField("firstName");
+        field.setAccessible(true);
+        field.set(pojo, "test");
+
+        //when
+        final String result = pojo.getFirstName();
+
+        //then
+        assertEquals("test", result, "magic_values");
     }
 
     @Test
-    void setFirstName() {
+    void setFirstName() throws NoSuchFieldException, IllegalAccessException {
+        final Administrator pojo = new Administrator();
+
+        //when
+        pojo.setFirstName("test");
+
+        //then
+        final Field field = pojo.getClass().getDeclaredField("firstName");
+        field.setAccessible(true);
+        assertEquals("test", field.get(pojo), "foo");
     }
 
     @Test
-    void getLastName() {
+    void getLastName() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Administrator pojo = new Administrator();
+        final Field field = pojo.getClass().getDeclaredField("lastName");
+        field.setAccessible(true);
+        field.set(pojo, "test");
+
+        //when
+        final String result = pojo.getLastName();
+
+        //then
+        assertEquals("test", result, "magic_values");
     }
 
     @Test
-    void setLastName() {
+    void setLastName() throws NoSuchFieldException, IllegalAccessException {
+        final Administrator pojo = new Administrator();
+
+        //when
+        pojo.setLastName("test");
+
+        //then
+        final Field field = pojo.getClass().getDeclaredField("lastName");
+        field.setAccessible(true);
+        assertEquals("test", field.get(pojo), "foo");
     }
 
     @Test
-    void getEmail() {
+    void getEmail() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Administrator pojo = new Administrator();
+        final Field field = pojo.getClass().getDeclaredField("email");
+        field.setAccessible(true);
+        field.set(pojo, "test");
+
+        //when
+        final String result = pojo.getEmail();
+
+        //then
+        assertEquals("test", result, "magic_values");
     }
 
     @Test
-    void setEmail() {
+    void setEmail() throws NoSuchFieldException, IllegalAccessException {
+        final Administrator pojo = new Administrator();
+
+        //when
+        pojo.setEmail("test");
+
+        //then
+        final Field field = pojo.getClass().getDeclaredField("email");
+        field.setAccessible(true);
+        assertEquals("test", field.get(pojo), "foo");
     }
 
     @Test
-    void getPassword() {
+    void getPassword() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Administrator pojo = new Administrator();
+        final Field field = pojo.getClass().getDeclaredField("password");
+        field.setAccessible(true);
+        field.set(pojo, "test");
+
+        //when
+        final String result = pojo.getPassword();
+
+        //then
+        assertEquals("test", result, "magic_values");
     }
 
     @Test
-    void setPassword() {
+    void setPassword() throws NoSuchFieldException, IllegalAccessException {
+        final Administrator pojo = new Administrator();
+
+        //when
+        pojo.setPassword("test");
+
+        //then
+        final Field field = pojo.getClass().getDeclaredField("password");
+        field.setAccessible(true);
+        assertEquals("test", field.get(pojo), "foo");
     }
 }
