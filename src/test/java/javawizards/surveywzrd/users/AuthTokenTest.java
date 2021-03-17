342d2fd1895f60a9f5faf945844003bd59e88c4b
Mauritz Merk <wi19181@lehre.dhbw-stuttgart.de>
2021-03-17 12:15:22 +0100

    Commit full test for Authtoken class. Problems with testing classvariable Adminstrator admin (type?).

--- src/test/java/javawizards/surveywzrd/users/AuthTokenTest.java
+++ src/test/java/javawizards/surveywzrd/users/AuthTokenTest.java
@@ -2,13 +2,15 @@ package javawizards.surveywzrd.users;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuthTokenTest {
 
     private AuthToken authtoken;
-
     @BeforeEach
     void setUp() throws Exception{
         authtoken = new AuthToken();
@@ -17,4 +19,60 @@ class AuthTokenTest {
     @AfterEach
     void tearDown() {
     }
+
+    @Test
+    void getAuthKey() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final AuthToken pojo = new AuthToken();
+        final Field field = pojo.getClass().getDeclaredField("authKey");
+        field.setAccessible(true);
+        field.set(pojo, "test");
+
+        //when
+        final String result = pojo.getAuthKey();
+
+        //then
+        assertEquals("test", result, "magic_values");
+    }
+
+    @Test
+    void setAuthKey() throws NoSuchFieldException, IllegalAccessException {
+        final AuthToken pojo = new AuthToken();
+
+        //when
+        pojo.setAuthKey("test");
+
+        //then
+        final Field field = pojo.getClass().getDeclaredField("authKey");
+        field.setAccessible(true);
+        assertEquals("test", field.get(pojo), "foo");
+    }
+
+   /* @Test
+    void getAdmin() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final AuthToken pojo = new AuthToken();
+        final Field field = pojo.getClass().getDeclaredField("admin");
+        field.setAccessible(true);
+        field.set(pojo, "test");
+
+        //when
+        final Administrator result = pojo.getAdmin();
+
+        //then
+        assertEquals("test", result, "magic_values");
+    }
+
+    @Test
+    void setAdmin() throws NoSuchFieldException, IllegalAccessException {
+        final AuthToken pojo = new AuthToken();
+
+        //when
+        pojo.setAdmin();
+
+        //then
+        final Field field = pojo.getClass().getDeclaredField("admin");
+        field.setAccessible(true);
+        assertEquals("test", field.get(pojo), "foo");
+    } */
 }
