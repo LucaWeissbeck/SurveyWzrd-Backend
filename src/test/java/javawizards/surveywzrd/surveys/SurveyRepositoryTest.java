50cccd3e5da51edced9ee961434c065f002d1dde
Daniel Ebert <wi19281@lehre.dhbw-stuttgart.de>
2021-05-03 23:14:32 +0200

    Test Changes

--- src/test/java/javawizards/surveywzrd/surveys/SurveyRepositoryTest.java
+++ src/test/java/javawizards/surveywzrd/surveys/SurveyRepositoryTest.java
@@ -15,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -27,6 +28,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class SurveyRepositoryTest {
 
