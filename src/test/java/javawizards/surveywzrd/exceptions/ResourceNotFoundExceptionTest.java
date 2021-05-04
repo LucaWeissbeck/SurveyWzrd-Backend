d5364861520b1aa8858016475ae9a18d9a74d060
Mauritz Merk <wi19181@lehre.dhbw-stuttgart.de>
2021-05-04 16:37:10 +0200

    ResourceNotFoundExceptionTest failt mit "expected: 404 - actual: 403"

--- src/test/java/javawizards/surveywzrd/exceptions/ResourceNotFoundExceptionTest.java
+++ src/test/java/javawizards/surveywzrd/exceptions/ResourceNotFoundExceptionTest.java
@@ -1,25 +1,40 @@
 package javawizards.surveywzrd.exceptions;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class ResourceNotFoundExceptionTest {
 
-    private ResourceNotFoundException resourcenotfoundexception;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp() {
-        System.out.println("setUp");
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+
+    @Test
+    void ResourceNotFoundException() throws Exception{
+        mockMvc.perform(get("/api/survey/getAll"))
+                .andExpect(status().isNotFound());
     }
 
-    @AfterEach
-    void tearDown() {
-        System.out.println("tearDown");
-    }
 
 
 }
