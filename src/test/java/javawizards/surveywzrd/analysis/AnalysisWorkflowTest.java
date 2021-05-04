cde99ea7d60758f3bec7d6c6f474fe647722f2c5
Kathrin <elja2043@gmail.com>
2021-05-04 22:58:33 +0200

    Filled Result-Test-Class

--- src/test/java/javawizards/surveywzrd/analysis/AnalysisWorkflowTest.java
+++ src/test/java/javawizards/surveywzrd/analysis/AnalysisWorkflowTest.java
@@ -1,25 +1,26 @@
 package javawizards.surveywzrd.analysis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javawizards.surveywzrd.SurveywzrdTestUtils;
 import javawizards.surveywzrd.surveys.AnswerOptionRepository;
-import javawizards.surveywzrd.surveys.Survey;
 import javawizards.surveywzrd.surveys.SurveyRepository;
-import javawizards.surveywzrd.users.*;
+import javawizards.surveywzrd.users.AdministratorRepository;
+import javawizards.surveywzrd.users.AuthTokenRepository;
+import javawizards.surveywzrd.users.ParticipantRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
@@ -33,52 +34,44 @@ public class AnalysisWorkflowTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private SurveyRepository surveyRepository;
-
-    @Autowired
-    private AdministratorRepository administratorRepository;
-
-    @Autowired
-    private AuthTokenRepository authTokenRepository;
-
-    @Autowired
-    private ParticipantRepository participantRepository;
-
-    @Autowired
-    private AnswerOptionRepository answerOptionRepository;
+    private SurveywzrdTestUtils surveywzrdTestUtils;
 
 
     @BeforeEach
     void setUp() {
-        Administrator toinsert = new Administrator("test@test.de", "test", true);
-        toinsert.setId(1L);
-        administratorRepository.save(toinsert);
-        authTokenRepository.save(new AuthToken("testkey", administratorRepository.findById(1L)));
+        surveywzrdTestUtils.createAdministratorAndAuthToken(1L, "String email");
+        surveywzrdTestUtils.createSurveyAnd3AnswerOptions(1L, 1L);
+        surveywzrdTestUtils.vote();
     }
 
 
     @Test
-    void createSurveyWorksCompleteley() throws Exception {
-        Survey surveyToInsert = new Survey("name", "description", new Date(), "question", true, "companyName");
+    void getResultsForAdmin() throws Exception {
 
-        mockMvc.perform(post("/api/survey/", 42L)
-                .contentType("application/json")
-                .header("x-api-key",authTokenRepository.findByAdminId(1L).get().getAuthKey())
-                //.param("sendWelcomeMail", "true")
-                .content(objectMapper.writeValueAsString(surveyToInsert)))
-                .andExpect(status().isOk());
-
-        Survey surveyToCheck = surveyRepository.findById(1L);
-        assertEquals(surveyToInsert.getDescription(), surveyToCheck.getDescription());
+        mockMvc.perform(get("/api/analysis/public/rawdata/{surveyID}", 1L)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                //.andExpect(jsonPath("$.accountId").value(12345))
+                //.andExpect(jsonPath("$.accountType").value("SAVINGS"))
+                //.andExpect(jsonPath("$.balance").value(5000.0));
 
     }
 
     @Test
-    void testSurveyAnswerOptions() throws Exception {
+    void getResultsForParticipant() throws Exception {
+        mockMvc.perform(get("/api/analysis/public/minimizedrawdata/{surveyID}", 1L)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
 
+    @Test
+    void getResultsForSurvey() throws Exception {
+        mockMvc.perform(get("/api/analysis/public/{surveyID}", 1L)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
 }
